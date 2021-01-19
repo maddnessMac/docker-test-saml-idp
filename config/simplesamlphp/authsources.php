676787880354a94e2edf87fb19ab03fd06dac38d
maddnessMac <daviann@gmail.com>
2021-01-19 21:42:20 +0000

    Update authsources.php
    Adding UserID for response

--- config/simplesamlphp/authsources.php
+++ config/simplesamlphp/authsources.php
@@ -8,10 +8,10 @@ $config = array(
 
     'example-userpass' => array(
         'exampleauth:UserPass',
-        'user1:user1pass' => array('uid' => array('1'),'role' => array('group1'),'email' => 'user1@example.com','RealName'=> 'User 1',),
-        'user2:user2pass' => array('uid' => array('2'),'role' => array('group2'),'email' => 'user2@example.com','RealName'=> 'User 2',),
-        'user3:user3pass' => array('uid' => array('3'),'role' => array('group2','group3'),'email' => 'user3@example.com','RealName'=> 'User 3',),
-        'user3:user4pass' => array('uid' => array('4'),'role' => array('group3','group4'),'email' => 'user4@example.com','RealName'=> 'User 4',),
+        'user1:user1pass' => array('uid' => array('1'),'role' => array('group1'),'email' => 'user1@example.com','RealName'=> 'User 1','UserID' => 'User1',),
+        'user2:user2pass' => array('uid' => array('2'),'role' => array('group2'),'email' => 'user2@example.com','RealName'=> 'User 2','UserID' => 'User2',),
+        'user3:user3pass' => array('uid' => array('3'),'role' => array('group2','group3'),'email' => 'user3@example.com','RealName'=> 'User 3','UserID' => 'User3',),
+        'user3:user4pass' => array('uid' => array('4'),'role' => array('group3','group4'),'email' => 'user4@example.com','RealName'=> 'User 4','UserID' => 'User4',),
     ),
 
 );
