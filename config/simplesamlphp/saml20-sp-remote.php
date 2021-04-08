e7dc8cc961bf21731c141cb2bb943a686cc09379
maddnessMac <daviann@gmail.com>
2021-04-08 07:49:26 +0000

    Update saml20-sp-remote.php
    Adding certificate finger print - need to add getenv if this works

--- config/simplesamlphp/saml20-sp-remote.php
+++ config/simplesamlphp/saml20-sp-remote.php
@@ -8,4 +8,5 @@
 $metadata[getenv('SIMPLESAMLPHP_SP_ENTITY_ID')] = array(
     'AssertionConsumerService' => getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE'),
     'SingleLogoutService' => getenv('SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE'),
+    'certFingerprint' => '119b9e027959cdb7c662cfd075d9e2ef384e445f',
 );
