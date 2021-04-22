0bbccfbd8563148b478fa98e35f4f3e3fbb3acdd
maddnessMac <daviann@gmail.com>
2021-04-22 10:02:22 +0000

    Update authsources.php
    correcting user 4

--- config/simplesamlphp/authsources.php
+++ config/simplesamlphp/authsources.php
@@ -11,7 +11,7 @@ $config = array(
         'user1:user1pass' => array('uid' => array('User_1'),'role' => array('group1'),'email' => 'user1@example.com','RealName'=> 'User 1',),
         'user2:user2pass' => array('uid' => array('User_2'),'role' => array('group2'),'email' => 'user2@example.com','RealName'=> 'User 2',),
         'user3:user3pass' => array('uid' => array('User_3'),'role' => array('group2','group3'),'email' => 'user3@example.com','RealName'=> 'User3',),
-        'user3:user4pass' => array('uid' => array('User_4'),'role' => array('group3','group4'),'email' => 'user4@example.com','RealName'=> 'User 4',),
+        'user4:user4pass' => array('uid' => array('User_4'),'role' => array('group3','group4'),'email' => 'user4@example.com','RealName'=> 'User 4',),
     ),
 
 );
